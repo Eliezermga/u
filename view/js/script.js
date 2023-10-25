a545a7e559794b6e3890580f29933b31b0fbbf55
UNIVERS GROUPE <149015596+universgroups@users.noreply.github.com>
2023-10-25 23:00:38 +0200

    univers grope

---
+++ view/js/script.js
@@ -0,0 +1,12 @@
+//nave barre 
+var mainListDiv = document.getElementById("mainListDiv"),
+    mediaButton = document.getElementById("mediaButton");
+
+mediaButton.onclick = function () {
+    
+    "use strict";
+    
+    mainListDiv.classList.toggle("show_list");
+    mediaButton.classList.toggle("active");
+    
+};
